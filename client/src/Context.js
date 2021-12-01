96555b4c1bcff9acd82de902e7802c8eae033916
Ed Tansey <office.fwd@yahoo.com>
2021-12-01 19:42:16 +0000

    Preserve the user state with cookies

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
 const Context = React.createContext(); 
@@ -12,6 +13,13 @@ export class Provider extends Component {
   constructor() {
     super();
     this.data = new Data();
+
+    this.cookie = Cookies.get('authenticatedUser');
+
+    this.state = {
+      authenticatedUser: this.cookie ? JSON.parse(this.cookie) : null
+    };
+
   }
 
   render() {
@@ -42,12 +50,18 @@ export class Provider extends Component {
           authenticatedUser : user,
         };
       });
+      Cookies.set('authenticatedUser', JSON.stringify(user), { expires: 1 });
     }
     return user;
   }
 
   signOut = () => {
-    this.setState({ authenticatedUser: null });
+    this.setState(() => {
+      return {
+        authenticatedUser: null,
+      };
+    });
+    Cookies.remove('authenticatedUser');
   }
 }
 
